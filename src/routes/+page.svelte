478117e5292034663b13c906639715ac51cf2d16
nitromelon <112331897+nitromelon@users.noreply.github.com>
2025-08-02 09:42:41 +0700

    revert

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,20 +6,15 @@
 	import Thuong_Unfolded from "$lib/components/pages/home/thuong_unfolded/thuong_unfolded.svelte";
 	import Loving_Words from "$lib/components/pages/home/loving_words/loving_words.svelte";
 	import YourThoughts from "$lib/components/pages/home/your_thoughts/your_thoughts.svelte";
-	import { currentIndex, isScrollProgress } from "$lib/components/pages/home/current_index.svelte";
 
 	export let menu = 0;
-
-	let header_height = 0;
-	$: hide_header = $isScrollProgress || $currentIndex != 0;
-	$: offset_top = -header_height * (hide_header ? 1 : 0);
 </script>
 
 <svelte:head>
 	<title>Chữ và Nghĩa</title>
 </svelte:head>
 
-<header bind:offsetHeight={header_height} style:transform="translateY({offset_top}px)">
+<header>
 	<div class="translate-wrapper">
 		<button
 			onclick={() => {
@@ -115,7 +110,7 @@
 	<hr style="border-top: 1px solid beige;" />
 </header>
 
-<section style:transform="translateY({offset_top / 2}px)">
+<section>
 	{#if menu === 1}
 		<About />
 	{:else if menu === 2}
