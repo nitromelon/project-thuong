58f5bd28aa1f63a5b53d895d50a288c112dba114
Đức Anh Trần <110408669+davinci-code97@users.noreply.github.com>
2025-08-05 12:53:13 +0700

    + fick

--- src/lib/components/pages/home/thuong_unfolded/pages/Page27.svelte
+++ src/lib/components/pages/home/thuong_unfolded/pages/Page27.svelte
@@ -138,7 +138,7 @@
 		box-shadow:
 			0 10px 30px rgba(0, 0, 0, 0.3),
 			inset 0 1px 0 rgba(255, 255, 255, 0.1);
-		transform: rotateY(-140deg) translateZ(20px);
+		transform: rotateY(-140deg);
 	}
 
 	.book-cover2::before {
